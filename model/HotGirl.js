29bdf381c0e1f32174cd61b42e6e8906dd13db60
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-27 14:30:24 +0700

    class

--- model/HotGirl.js
+++ model/HotGirl.js
@@ -1,7 +1,17 @@
 class HotGirl {
     constructor(name, age, image) {
-
+        this.name = name;
+        this.age = age;
+        this.image = image;
     }
 }
 
-const arrHotGirl = [];
+const arrHotGirl = [
+    new HotGirl('Ely Tran', 18, '1.jpg'),
+    new HotGirl('Ha Ho', 30, '2.jpg'),
+    new HotGirl('Thuy Chi', 31, '3.jpg'),
+    new HotGirl('Chi Pu', 32, '4.jpg'),
+    new HotGirl('Tam Tit', 32, '5.jpg')
+];
+
+module.exports = arrHotGirl;
