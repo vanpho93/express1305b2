a3b4fe5b5137372aa66b2e53cd83ce3db31f2fde
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-03 14:11:36 +0700

    xong

--- db.js
+++ db.js
@@ -10,9 +10,43 @@ const pool = new pg.Pool({
     idleTimeoutMillis: 1000
 });
 
-pool.connect((err, client, done) => {
-    client.query('SELECT * FROM "HotGirl"', (errQuery, result) => {
-        console.log(result.rows);
-        done();
+// pool.connect((err, client, done) => {
+//     if (err) return console.log(err.toString());
+//     client.query('SELECT * FROM "HotGirl"', (errQuery, result) => {
+//         if (errQuery) return console.log(errQuery);
+//         console.log(result.rows);
+//         done();
+//     });
+// });
+
+function queryDB(sql, cb) {
+    pool.connect((err, client, done) => {
+        if (err) return cb(err);
+        client.query(sql, (errQuery, result) => {
+            if (errQuery) return cb(errQuery);
+            done(errQuery);
+            cb(null, result.rows);
+        });
     });
-});
+}
+
+// queryDB('SELECT * FROM "HotGirl"', function(e, r) {
+//     if (e) return console.log(e + '');
+//     console.log(r);
+// });
+
+// function onSuccess(kq) {
+//     console.log(kq);
+// }
+
+// const arrowSuccess = kq => console.log(kq);
+
+// queryDB('SELECT * FROM "HotGirl"', kq => console.log(kq));
+
+// queryDB('SELECT * FROM "HotGirl"', function() {
+//     console.log(kq);
+// });
+
+// queryDB('SELECT * FROM "HotGirl"', onSuccess);
+
+// queryDB('SELECT * FROM "HotGirl"', arrowSuccess);
