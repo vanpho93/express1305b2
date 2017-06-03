5c0883d24642124843c6fac538d6e115f004681b
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-03 10:22:26 +0700

    add new girl

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require('express');
+const parser = require('body-parser').urlencoded({ extended: false });
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-const arrHotGirl = require('./model/HotGirl');
-
 app.get('/', (req, res) => res.render('home', { mang: arrHotGirl }));
 
-app.get('/admin', (req, res) => res.render('admin', { isAdmin: false }));
+app.get('/admin', (req, res) => res.render('admin'));
 
 app.get('/show/:index', (req, res) => {
     const { index } = req.params;
     res.render('show', { hotGirl: arrHotGirl[index], index });
 });
 
+app.post('/admin/add', parser, (req, res) => {
+    const { name, image, age } = req.body;
+    const hotGirl = new HotGirl(name, age, image);
+    arrHotGirl.push(hotGirl);
+    res.send('Them thanh cong!!!');
+});
+
 app.listen(process.env.PORT || 3000);
+
+class HotGirl {
+    constructor(name, age, image) {
+        this.name = name;
+        this.age = age;
+        this.image = image;
+    }
+}
+
+const arrHotGirl = [
+    new HotGirl('Ely Tran', 18, '1.jpg'),
+    new HotGirl('Ha Ho', 30, '2.jpg')
+];
